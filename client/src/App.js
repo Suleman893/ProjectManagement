1a7c41f6de3c6dd5f0d9a259e22549dc1a0daebf
suleman893 <sulemanahmad893@gmail.com>
2023-11-05 23:42:33 +0500

    Create client done

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,38 @@
 import Client from "./components/Client";
 import Header from "./components/Headers";
 import { ApolloProvider, InMemoryCache, ApolloClient } from "@apollo/client";
+import CreateClientModal from "./components/CreateClientModal";
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  cache,
+});
 
 function App() {
-  const client = new ApolloClient({
-    uri: "http://localhost:5000/graphql",
-    cache: new InMemoryCache(),
-  });
-
   return (
     <ApolloProvider client={client}>
       <Header />
       <div className="container">
+        <CreateClientModal />
         <Client />
       </div>
     </ApolloProvider>
