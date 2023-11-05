eb3652b8532b138e10c651660b81ed413263c086
suleman893 <sulemanahmad893@gmail.com>
2023-11-05 15:23:29 +0500

    Graphql server

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+require("dotenv").config();
+const colors = require("colors");
+const schema = require("./schema/schema");
+//Graphql
+const { graphqlHTTP } = require("express-graphql");
+const connectDB = require("./config/db");
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+//Connect to DB
+connectDB();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development" ? true : false,
+  })
+);
+
+app.listen(port, console.log("Server running on PORT", port));
